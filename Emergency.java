d39015c482cc1525028c1b760c17fea5af955599
khushi bansal <khushibansal1048@gmail.com>
2024-11-17 01:39:16 +0530

    Create Emergency.java

---
+++ Emergency.java
@@ -0,0 +1,8 @@
+// src/Emergency.java
+public class Emergency {
+    public static void handleEmergency() {
+        System.out.println("\nEmergency mode activated.");
+        System.out.println("Emergency service contacted. Help is on the way.");
+        // Add more emergency handling logic as needed
+    }
+}
